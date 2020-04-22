92dcb5f8a632967b6c0c30cd1d6359d9a93d6a7a
Kristine Robison <krobison@gmail.com>
2020-04-22 10:54:50 -0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  defaults format: :json do
+    resources :users
+    resources :lists do
+      resources :items
+    end
+    resources :user_sessions
+
+    root to: 'user_sessions#new'
+  end
 end
