ef5ba7f77c15dbd36b10a7261a192935f843c187
Kristine Robison <krobison@gmail.com>
2020-09-05 12:57:53 -0700

    FIX: Remove users from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   defaults format: :json do
-    resources :users do
-      resources :lists do
-        member do
-          post 'share/:user_id', to: 'lists#share'
-        end
-        resources :items
+    resources :lists do
+      member do
+        post '/share/:user_id', to: 'lists#share'
       end
+      resources :items
     end
 
     post 'authenticate', to: 'authentication#authenticate'
